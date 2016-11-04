8ddd7d827c1bfd8b996a90500535af5cbb0ee91e
Giovane Menezes <giovane.menezes@live.com>
2016-11-03 23:40:23 -0400

    Organização de pastas

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "arvore234.h"
+
+using namespace std;
+
+int main() {
+
+	//Isso é uma Instancia da classe
+	Arvore234 arvore234 = Arvore234();
+
+
+	//Inserindo valores na Árvore
+	arvore234.inserir(10);
+	arvore234.inserir(30);
+	arvore234.inserir(50);
+	//arvore234.inserir(15);
+	//arvore234.inserir(18);
+	//arvore234.inserir(55);
+	//arvore234.inserir(60);
+	//arvore234.inserir(32);
+	//arvore234.inserir(51);
+	//arvore234.inserir(61);
+	//arvore234.inserir(62);
+	
+	return 0;
+}
