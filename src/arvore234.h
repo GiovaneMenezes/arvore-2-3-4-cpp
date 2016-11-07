79256d78e842980887c276364aaf4add9b01851c
David Camurça <david.camurcaa@gmail.com>
2016-11-07 14:40:52 -0200

    Atualização de comentários

--- src/arvore234.h
+++ src/arvore234.h
@@ -8,6 +8,7 @@
     *       David Camurça  - Engenharia de Software                 *
 *********************************************************************/
 
+//Estrutura da Arvore 2-3-4
 typedef struct no {
 	struct no* pai;
 	int dados[3];
@@ -15,24 +16,26 @@ typedef struct no {
 	struct no* ponteiros[4];
 } NO;
 
+
+//Classe da Arvore 2-3-4
 class Arvore234{
 	public:
 		Arvore234();
-		void inserir(int);//**
+		void inserir(int);
 		void imprime();
 		void remover(int);
 		bool buscaValor(int);
 
 	private:
 		NO *raiz;
-		NO *busca(int); //**
-		void insere(NO**, int); //**
-		void divide(NO**); //**
-		bool estanoNo(NO**, int); //**
-		bool eRaiz(NO**); //**
-		bool eFolha(NO**); //**
-		NO *encontraNoFilho(NO**,int); //**
-		void inserirValor(NO**, int);//**
+		NO *busca(int);
+		void insere(NO**, int);
+		void divide(NO**);
+		bool estanoNo(NO**, int);
+		bool eRaiz(NO**);
+		bool eFolha(NO**);
+		NO *encontraNoFilho(NO**,int);
+		void inserirValor(NO**, int);
 		void imprime(NO*);
 		void bubblesort(NO**);
 		NO *encontraSucessor(NO*, int);
