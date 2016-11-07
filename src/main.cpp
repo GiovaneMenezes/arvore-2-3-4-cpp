880d4d3f318ee1d07257cb6513aa3fc6f9c18e80
David Camurça <david.camurcaa@gmail.com>
2016-11-07 14:34:04 -0200

    Atualização de comentários

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+/********************************************************************
+    * Implementação Arvore 2-3-4 em C++ Insercao, Busca e Remocao   *
+    * Estrutura de dados e Algoritmos III                           *
+    * Professor: Antonio Ramos de Carvalho Junior                   *
+    * FUCAPI - 07/11/2016 - Manaus-AM                               *
+    *                                                               *
+    * Team: Giovane Menezes - Engenharia de Computacao              *
+    *       David Camurça  - Engenharia de Software                 *
+*********************************************************************/
+
 #include <iostream>
 #include "arvore234.h"
 #include <stdlib.h>
@@ -11,14 +21,14 @@ int main() {
 
 
 	//Inserindo valores na Árvore
-	for(int i=1;i<=100000000;i++){
+	for(int i=1;i<5;i++){
 		arvore234.inserir(i);
 	}
-	//arvore234.remover(7);
-	//arvore234.imprime();
-	
+	arvore234.remover(2);
+	arvore234.imprime();
+
 	cout << "Arvore construida" << endl;
-	
+
 	if(arvore234.buscaValor(54678))
 		cout << "Encontrou!!" << endl;
 	else
